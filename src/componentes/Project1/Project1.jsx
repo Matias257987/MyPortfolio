5e344ffad86134753b9d06b0ef24b720353ce8d6
Matias <acostamatias25@gmail.com>
2023-02-27 09:50:24 -0300

    first commit

---
+++ src/componentes/Project1/Project1.jsx
@@ -0,0 +1,10 @@
+import { Col } from "react-bootstrap";
+import { ProjectCard } from "../ProjectCard/ProjectCard";
+
+export const Project1 = () => {
+  return (
+    <>
+      <ProjectCard />
+    </>
+  );
+};
