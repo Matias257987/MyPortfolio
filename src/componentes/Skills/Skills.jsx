53563b0f5792d680f38ebcaa02e9363f6c16b5e0
Matias <acostamatias25@gmail.com>
2023-02-27 09:49:44 -0300

    Skills completo

--- src/componentes/Skills/Skills.jsx
+++ src/componentes/Skills/Skills.jsx
@@ -1,9 +1,17 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from "../../assets/img/meter1.svg";
-import meter2 from "../../assets/img/meter2.svg";
-import meter3 from "../../assets/img/meter3.svg";
+import express from "../../assets/img/express.svg";
+import js from "../../assets/img/js.svg";
+import html from "../../assets/img/html.svg";
+import css from "../../assets/img/css.svg";
+import react from "../../assets/img/react.svg";
+import redux from "../../assets/img/redux.svg";
+import node from "../../assets/img/node.svg";
+import postgresql from "../../assets/img/postgresql.svg";
+import sequelize from "../../assets/img/sequelize.svg";
+import git from "../../assets/img/git.svg";
+import bootstrap from "../../assets/img/bootstrap.svg";
 import colorSharp from "../../assets/img/color-sharp.png";
 
 export const Skills = () => {
@@ -34,10 +42,13 @@ export const Skills = () => {
             <div className="skill-bx">
               <h2>Skills</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />
-                Placeat consectetur recusandae aliquam dolorem quo maxime <br />
-                molestiae eius quidem nostrum aperiam nemo voluptate harum,
-                <br /> nisi deleniti perspiciatis, odit eligendi velit natus.
+                Soy una persona con habilidades tech de fullstack web developer,
+                como React, Redux, PostgreSQL, Node js, HTML, CSS, Express, Java
+                Script y Sequelize. Y habilidades soft, como resiliente,
+                comprometido, apasionado, pensamiento analítico, en búsqueda
+                activa de resultados y resolución de problemas, orientación,
+                integración, trabajo en equipo, empatía, iniciativa, curiosidad
+                y dedicación
               </p>
               <Carousel
                 responsive={responsive}
@@ -45,23 +56,55 @@ export const Skills = () => {
                 className="skill-slider"
               >
                 <div className="item">
-                  <img src={meter1} alt="image" />
-                  <h5>Web Developer</h5>
+                  <img src={react} alt="" />
+                  <h5>React js</h5>
                 </div>
                 <div className="item">
-                  <img src={meter2} alt="image" />
-                  <h5>Brand Identity</h5>
+                  <img src={js} alt="" />
+                  <h5>JavaScript</h5>
                 </div>
                 <div className="item">
-                  <img src={meter3} alt="image" />
-                  <h5>Logo Desing</h5>
+                  <img src={express} alt="" />
+                  <h5>Express js</h5>
+                </div>
+                <div className="item">
+                  <img src={redux} alt="" />
+                  <h5>Redux js</h5>
+                </div>
+                <div className="item">
+                  <img src={node} alt="" />
+                  <h5>Node js</h5>
+                </div>
+                <div className="item">
+                  <img src={postgresql} alt="" />
+                  <h5>PostgreSQL</h5>
+                </div>
+                <div className="item">
+                  <img src={css} alt="" />
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={html} alt="" />
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <img src={bootstrap} alt="" />
+                  <h5>Bootstrap</h5>
+                </div>
+                <div className="item">
+                  <img src={sequelize} alt="" />
+                  <h5>Sequelize</h5>
+                </div>
+                <div className="item">
+                  <img src={git} alt="" />
+                  <h5>Git</h5>
                 </div>
               </Carousel>
             </div>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-left" src={colorSharp} />
+      <img className="background-image-left" src={colorSharp} alt="" />
     </section>
   );
 };
